4db3e1ac5fd2cfa9facb6e614bda9325bae9d9de
Haylemicheal <hmicheal50@gmail.com>
2019-05-26 01:12:25 +0300

    modify the socket program using cpp

---
+++ newclient.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include "newclient.h"
+int main(int argc, char const *argv[])
+{
+   /* code */
+   if (argc < 3) {
+      fprintf(stderr,"usage %s hostname port\n", argv[0]);
+      exit(0);
+   }
+   
+   int portno = atoi(argv[2]);
+   Client newclient;
+   char bufer[256];
+
+   int sockfd = newclient.socket_(AF_INET, SOCK_STREAM, 0,portno);
+   
+    newclient.connect_(argv[1]);
+    newclient.read_msg();
+    printf("Message: ");
+    scanf("%s",bufer);
+    newclient.write_msg(bufer);
+    newclient.read_msg();
+    printf("Message: ");
+    scanf("%s",bufer);
+    newclient.write_msg(bufer);
+    newclient.read_msg();
+    printf("Message: ");
+    scanf("%s",bufer);
+    newclient.write_msg(bufer);
+   
+    
+    
+   return 0;
+}
---
+++ newserver.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string.h>
+#include "newserver.h"
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	/* code */
+    int n;
+    char bufer[256];
+    Server newserve;
+    newserve.socket_(AF_INET, SOCK_STREAM, 0,5001);
+    
+    newserve.read_msg();
+    printf("Message: ");
+    scanf("%s",bufer);
+    newserve.write_msg(bufer,strlen(bufer));
+    newserve.read_msg();
+    printf("Message: ");
+    scanf("%s",bufer);
+    newserve.write_msg(bufer,strlen(bufer));
+    newserve.read_msg();
+    printf("Message: ");
+    scanf("%s",bufer);
+    newserve.write_msg(bufer,strlen(bufer));
+	return 0;
+}
